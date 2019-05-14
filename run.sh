e8268e3e5fc1a1f49e9f49c7bd43048d0a6c728e
Michael J. Wilson <wilsongis@gmail.com>
2019-05-14 09:05:58 -0500

    Setting up project

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t dashboard -f Dockerfile .
-docker run --privileged -ti -v ${PWD}:/usr/local/bin/dashboard -w /usr/local/bin/dashboard -p 3001:3001 dashboard zsh
+docker build -t census-view -f Dockerfile .
+docker run --privileged -ti -v ${PWD}:/usr/local/bin/census-view -w /usr/local/bin/census-view -p 3001:3001 census-view zsh
